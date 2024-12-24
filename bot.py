2737235b0b1bf333501ac5f77b88dd646c56cb60
CEPIbN <cepibn.grey@gmail.com>
2024-12-24 15:49:23 +0500

    Add comments

--- bot.py
+++ bot.py
@@ -2,20 +2,24 @@ import discord
 from discord.ext import commands
 from config import settings
 
+# Создаем переменные, значение которых находится в файле конфига.
 TOKEN = settings['TOKEN']
 PREFIX = settings['PREFIX']
 ROLE = settings['ROLE']
-intents = discord.Intents().all()
 
+# Создаем права боту
+intents = discord.Intents().all()
 
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 
+# Создаем команду, которая при в воде будет выводить сообщение
 @bot.command()
 async def hello(ctx):
     await ctx.reply("Привет :)")
 
 
+# Бот назначает роль новому участнику сервера
 @bot.event
 async def on_member_join(member):
     join_role = discord.utils.get(member.guild.roles, name=ROLE)
